f22ed47e09e905c70d0755b63e2149b48f76e4e7
LEAKONO <leakonoemmanuel3@gmail.com>
2025-12-01 16:41:10 +0300

    Add Django project configuration and core app setup

---
+++ climate/__init__.py
@@ -0,0 +1,2 @@
+# This file makes the climate directory a Python package
+default_app_config = 'climate.apps.ClimateConfig'
--- climate/apps.py
+++ climate/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ClimateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'climate'
+    
+    def ready(self):
+        # Import signals
+        import climate.signals
